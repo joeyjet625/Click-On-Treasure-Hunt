dbd8ca57396461f460403687ba81241d32f9f667
joeyjet625 <44040581+joeyjet625@users.noreply.github.com>
2019-01-07 17:32:06 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,28 @@
+$("#image2").hide(); 
+$(".button").click(function() {
+$(".image2").show(); 
+$(".doubleclick").show();
+});
+
+$(".image2").dblclick(function() {
+$(".train").show();
+$(".Ntrain2").show();
+});
+
+$(".train").hover(function() {
+$(".j").show();
+});
+
+$(".j").dblclick(function() {
+$(".TimesSquare").show();
+$(".TimesSquare2").show();
+});
+
+$(".j").hover(function() {
+$(".ndave").show();
+$(".desination").show();
+});
+
+
+
+
